af668ceeb3ab7b470440675167186e7f624f830c
Bracha Ginsberger <gb@sem-wolf.org>
2020-09-05 23:49:51 +0300

    add crop image function around given cordinate

--- code/script/create_sample.py
+++ code/script/create_sample.py
@@ -17,6 +17,14 @@ def is_tfl_divide(x_axis: np.ndarray, y_axis: np.ndarray, label: np.ndarray):
     return x_true, y_true, x_false, y_false
 
 
+def crop_image(img: np.ndarray, x: int, y: int):
+    crop_size = 40
+    h, w, d = img.shape
+    padded_img = np.zeros((crop_size*2+h, crop_size*2+w, 3), dtype=np.uint8)
+    padded_img[crop_size: -crop_size, crop_size: -crop_size] = img
+    cropped = padded_img[y: y+crop_size*2+1, x: x+crop_size*2+1]
+    return cropped
+
 
 
 
