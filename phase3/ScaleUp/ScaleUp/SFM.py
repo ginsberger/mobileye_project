a112c95a3791dd2823e8e694b03d7a33e570efa1
Bracha Ginsberger <gb@sem-wolf.org>
2020-09-06 13:27:18 +0300

    phase3 - distance calculation implement normalize function

---
+++ phase3/ScaleUp/ScaleUp/SFM.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+def calc_TFL_dist(prev_container, curr_container, focal, pp):
+    norm_prev_pts, norm_curr_pts, R, foe, tZ = prepare_3D_data(prev_container, curr_container, focal, pp)
+    if(abs(tZ) < 10e-6):
+        print('tz = ', tZ)
+    elif (norm_prev_pts.size == 0):
+        print('no prev points')
+    elif (norm_prev_pts.size == 0):
+        print('no curr points')
+    else:
+        curr_container.corresponding_ind, curr_container.traffic_lights_3d_location, curr_container.valid = calc_3D_data(norm_prev_pts, norm_curr_pts, R, foe, tZ)
+    return curr_container
+
+
+def prepare_3D_data(prev_container, curr_container, focal, pp):
+    norm_prev_pts = normalize(prev_container.traffic_light, focal, pp)
+    norm_curr_pts = normalize(curr_container.traffic_light, focal, pp)
+    R, foe, tZ = decompose(np.array(curr_container))
+    return norm_prev_pts, norm_curr_pts, R, foe, tZ
+
+
+def calc_3D_data(norm_prev_pts, norm_curr_pts, R, foe, tZ):
+    norm_rot_pts = rotate(norm_prev_pts, R)
+    pts_3D = []
+    corresponding_ind = []
+    validVec = []
+    for p_curr in norm_curr_pts:
+        corresponding_p_ind, corresponding_p_rot = find_corresponding_points(p_curr, norm_rot_pts, foe)
+        Z = calc_dist(p_curr, corresponding_p_rot, foe, tZ)
+        valid = (Z > 0)
+        if not valid:
+            Z = 0
+        validVec.append(valid)
+        P = Z * np.array([p_curr[0], p_curr[1], 1])
+        pts_3D.append((P[0], P[1], P[2]))
+        corresponding_ind.append(corresponding_p_ind)
+    return corresponding_ind, np.array(pts_3D), validVec
+
+
+def normalize(pts, focal, pp):
+    # transform pixels into normalized pixels using the focal length and principle point
+
+    for pt in pts:
+        pt[0] = (pt[0] - pp[0]) / focal
+        pt[1] = (pt[1] - pp[1]) / focal
+
+    return pts
+
+
+def unnormalize(pts, focal, pp):
+    # transform normalized pixels into pixels using the focal length and principle point
+    pass
+
+
+def decompose(EM):
+    # extract R, foe and tZ from the Ego Motion
+    pass
+
+
+def rotate(pts, R):
+    # rotate the points - pts using R
+    pass
+
+
+def find_corresponding_points(p, norm_pts_rot, foe):
+    # compute the epipolar line between p and foe
+    # run over all norm_pts_rot and find the one closest to the epipolar line
+    # return the closest point and its index
+    pass
+
+
+def calc_dist(p_curr, p_rot, foe, tZ):
+    # calculate the distance of p_curr using x_curr, x_rot, foe_x and tZ
+    # calculate the distance of p_curr using y_curr, y_rot, foe_y and tZ
+    # combine the two estimations and return estimated Z
+    pass
