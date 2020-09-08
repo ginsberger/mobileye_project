f03f069d859f24db3d7c7f1028775e79bc74785a
Bracha Ginsberger <gb@sem-wolf.org>
2020-09-08 20:24:34 +0300

    add TFLMan class and implement init and on_frame functions

---
+++ code/script/tfl_manager.py
@@ -0,0 +1,57 @@
+import pickle
+from numpy.random import randint
+from utils import Color
+import matplotlib.pyplot as plt
+from visualize_test import visualize
+
+
+class TFLMan:
+    frame_number = 0
+
+    def __init__(self, pkl_path):
+        with open(pkl_path, 'rb') as pklfile:
+            pkl_data = pickle.load(pklfile, encoding='latin1')
+
+        self.__EMs = {k: v for k, v in pkl_data.items() if 'egomotion' in k}
+        self.__pp = pkl_data['principle_point']
+        self.__focal = pkl_data['flx']
+        self.__prev_frame = None
+
+    def on_frame(self, current_frame):
+        candidates, auxilary = self.__get_candidates(current_frame)
+        traffic_lights, traffic_auxiliary = self.__get_tfl_coordinates(current_frame, candidates, auxilary)
+
+        current_frame = FrameContainer(current_frame)
+        if self.__prev_frame:
+            distance = self.__get_dists(self.__prev_frame, current_frame, self.__prev_frame.traffic_light, traffic_lights)
+            self.__prev_frame = current_frame
+
+        else:
+            distance = None
+            self.__prev_frame = current_frame
+
+        TFLMan.frame_number += 1
+        visualize(current_frame, candidates, auxilary, traffic_lights, traffic_auxiliary, distance)
+
+        return traffic_lights, traffic_auxiliary, distance
+
+    @staticmethod
+    def __get_candidates(image):
+        pass
+
+    @staticmethod
+    def __get_tfl_coordinates(image, candidates, auxilary):
+        pass
+
+    def __get_dists(self, prev_frame, current_frame, prev_traffic_lights, current_traffic_lights):
+        pass
+
+
+class FrameContainer(object):
+    def __init__(self, img_path):
+        self.img = plt.imread(img_path)
+        self.traffic_light = []
+        self.traffic_lights_3d_location = []
+        self.EM = []
+        self.corresponding_ind = []
+        self.valid = []
